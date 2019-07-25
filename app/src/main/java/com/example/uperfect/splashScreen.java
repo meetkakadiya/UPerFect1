9418a5206e329df3df21cc7df9143d7fed41f2c7
meetmk8985 <meetkakadiya710@gmail.com>
2019-07-26 01:28:28 +0530

    All Images Error are solved

--- app/src/main/java/com/example/uperfect/splashScreen.java
+++ app/src/main/java/com/example/uperfect/splashScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class splashScreen extends AppCompatActivity {
     private static int wait = 2000;
